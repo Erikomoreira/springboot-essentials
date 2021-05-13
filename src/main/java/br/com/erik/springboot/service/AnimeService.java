175d780ad067f15d7eeac8fe1d137fb94fce966f
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-12 22:44:33 -0300

    Spring Boot 2 Essentials 08 - Método GET parte 2

--- src/main/java/br/com/erik/springboot/service/AnimeService.java
+++ src/main/java/br/com/erik/springboot/service/AnimeService.java
@@ -1,16 +1,26 @@
 package br.com.erik.springboot.service;
 
 import br.com.erik.springboot.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
-    //private AnimeRepository animeRepository;
+    private List<Anime> animes = List.of(new Anime(1L,"Boku No Hero"), new Anime(2L,"Berserk"));
 
     public List<Anime> listAll(){
-        return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L,"Berserk"));
+        return animes;
     }
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
 }
