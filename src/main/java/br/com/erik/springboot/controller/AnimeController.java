517d0aaa6a6168217e2f3b1a647acb5aaa0f8af5
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-12 21:48:11 -0300

    Spring Boot 2 Essentials 06 - Gerando projeto com start.spring.io

---
+++ src/main/java/br/com/erik/springboot/controller/AnimeController.java
@@ -0,0 +1,28 @@
+package br.com.erik.springboot.controller;
+
+import br.com.erik.springboot.domain.Anime;
+import br.com.erik.springboot.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    @GetMapping("list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+    }
+
+}
