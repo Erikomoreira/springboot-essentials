3f4e1bdecf372e23dbdf59095a682a999f5d2157
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-16 15:32:11 -0300

    Spring Boot 2 Essentials 20 - Validação de campos

--- src/main/java/br/com/erik/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/br/com/erik/springboot/requests/AnimePostRequestBody.java
@@ -2,7 +2,10 @@ package br.com.erik.springboot.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class AnimePostRequestBody {
+    @NotBlank(message = "The name cannot be empty")
     private String name;
 }
