e2d21967f589cadd8e7bfb108406211fde8611c4
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-16 15:11:23 -0300

    Spring Boot 2 Essentials 18 - Handler Global

---
+++ src/main/java/br/com/erik/springboot/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,16 @@
+package br.com.erik.springboot.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+    private String title;
+    private int status;
+    private String details;
+    private String developMessage;
+    private LocalDateTime timestamp;
+}
