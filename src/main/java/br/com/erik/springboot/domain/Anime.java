a6de52330951b15196db08e957ebd46274b68e03
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-17 15:53:10 -0300

    Spring Boot 2 Essentials 32 - Spring Data JPA Test pt 03 - Exceções

--- src/main/java/br/com/erik/springboot/domain/Anime.java
+++ src/main/java/br/com/erik/springboot/domain/Anime.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -20,5 +21,6 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "The name cannot be empty")
     private String name;
 }
