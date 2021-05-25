57bb0910f4b51f485af5ea512352024e9dbf696f
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-24 23:44:26 -0300

    Spring Boot 2 Essentials 41 - Spring Security pt 02 - Segurança a nível de métodos com PreAuthorize

--- src/main/java/br/com/erik/springboot/config/SecurityConfig.java
+++ src/main/java/br/com/erik/springboot/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.erik.springboot.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
