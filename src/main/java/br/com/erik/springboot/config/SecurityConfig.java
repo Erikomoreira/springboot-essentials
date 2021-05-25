268992d8c489cb43b68f3a6871f5a5a4018ec1c6
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-24 21:42:22 -0300

    Spring Boot 2 Essentials 40 - Spring Security pt 01 - CSRF Token

--- src/main/java/br/com/erik/springboot/config/SecurityConfig.java
+++ src/main/java/br/com/erik/springboot/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                // csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
