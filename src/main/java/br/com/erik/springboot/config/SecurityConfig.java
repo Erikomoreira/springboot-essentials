df2bf5c331ca9cbb17de58d94ccea33310c72134
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-05-24 21:04:13 -0300

    Spring Boot 2 Essentials 39 - Spring Security pt 01 - Autenticação em memória

---
+++ src/main/java/br/com/erik/springboot/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.erik.springboot.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoded {}", passwordEncoder.encode("test"));
+        auth.inMemoryAuthentication()
+                .withUser("Erik")
+                .password(passwordEncoder.encode("test"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("Bob")
+                .password(passwordEncoder.encode("test"))
+                .roles("USER");
+
+    }
+
+}
